e31d24f1e15fa3d1a550881c48355eb5db50bdf3
Sathya Softech-in <sathyasoftech@outlook.com>
2026-02-08 16:12:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-// import "./index.css"; // global styles
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
